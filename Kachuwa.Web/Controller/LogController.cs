d407420a4cafb85f10ca78b5a433f9d7f452aaed
Tamang Binod <bakamaru_binod@hotmail.com>
2017-04-26 16:41:22 +0545

    log controller added

---
+++ Kachuwa.Web/Controller/LogController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kachuwa.Log;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kachuwa.Web
+{
+    //[Authorize(Roles = "Admin,Developer")]
+    public class LogController : BaseController
+    {
+        public ILoggerService LoggerService { get; }
+
+        public LogController(ILoggerService loggerService)
+        {
+            LoggerService = loggerService;
+        }
+        public IActionResult Index(int offset = 1, int limit = 20)
+        {
+            var dailyLogs = LoggerService.GetTodaysLogs(offset, limit);
+            return Json(dailyLogs.Logs);
+        }
+        public IActionResult ByDate(int offset = 1, int limit = 20,DateTime? day=null)
+        {
+            //day = day ?? DateTime.Now;
+            var dailyLogs = LoggerService.GetLogs(offset, limit, day.GetValueOrDefault(DateTime.Now));
+            return Json(dailyLogs.Logs);
+        }
+    }
+}
