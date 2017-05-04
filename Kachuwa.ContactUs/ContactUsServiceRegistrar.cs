bbf4c71feb0dd5ad6e702e30d23e095ed3b43e9f
Tamang Binod <bakamaru_binod@hotmail.com>
2017-05-04 17:46:47 +0545

    added module own resources

--- Kachuwa.ContactUs/ContactUsServiceRegistrar.cs
+++ Kachuwa.ContactUs/ContactUsServiceRegistrar.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System.Reflection;
 using Kachuwa.Core.DI;
 using Kachuwa.Web.Razor;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
-using System.Reflection;
-using Kachuwa.Web.Module;
 
 namespace Kachuwa.ContactUs
 {
@@ -26,14 +24,5 @@ namespace Kachuwa.ContactUs
                 opts.FileProviders.Add(assp);
             });
         }
-    }
-
-    public class ContactUsModule : IModule
-    {
-        public string Name { get; set; }="ContactUs";
-        public string Version { get; set; } = "1.0.0.0";
-        public List<string> SupportedVersions { get; set; }=new List<string>(){"1.0.0"};
-        public string Author { get; set; } = "Binod Tamang";
-        public Assembly Assembly { get; set; } = typeof(ContactUsModule).GetTypeInfo().Assembly;
     }
 }
