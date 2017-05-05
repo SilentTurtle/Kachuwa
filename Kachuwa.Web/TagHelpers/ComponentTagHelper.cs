2210ce7a1cc16fed46659a0d5659871b308bf0fc
Tamang Binod <bakamaru_binod@hotmail.com>
2017-05-05 17:03:15 +0545

    updated component taghelper and plugin tag helper

--- Kachuwa.Web/TagHelpers/ComponentTagHelper.cs
+++ Kachuwa.Web/TagHelpers/ComponentTagHelper.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Threading.Tasks;
+using Kachuwa.Log;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
@@ -10,6 +12,7 @@ namespace Kachuwa.Web.TagHelpers
     [HtmlTargetElement("component")]
     public class ComponentTagHelper : TagHelper
     {
+        private readonly ILogger _logger;
         private readonly DefaultViewComponentHelper _componentHelper;
 
         [HtmlAttributeName("name")]
@@ -21,18 +24,31 @@ namespace Kachuwa.Web.TagHelpers
         [ViewContext] // inform razor to inject
         public ViewContext ViewContext { get; set; }
 
-        public ComponentTagHelper(IViewComponentHelper componentHelper)
+        public ComponentTagHelper(IViewComponentHelper componentHelper, ILogger logger)
         {
+            _logger = logger;
             _componentHelper = componentHelper as DefaultViewComponentHelper;
         }
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            _componentHelper.Contextualize(ViewContext);
+            try
+            {
+                output.TagName = "div";
+                output.Attributes.Add("k-cn", Name);
+                output.Attributes.Add("class", "k-component");
+                _componentHelper.Contextualize(ViewContext);
+                output.Content.AppendHtml(
+                    await _componentHelper.InvokeAsync(Name, Params)
+                );
+            }
+            catch (Exception e)
+            {
+                output.Content.AppendHtml("Component Loading Error!See Logs.");
+                _logger.Log(LogType.Error, () => "Component Loading Error", e);
 
-            output.Content.AppendHtml(
-                await _componentHelper.InvokeAsync(Name, Params)
-            );
+
+            }
         }
     }
 }
