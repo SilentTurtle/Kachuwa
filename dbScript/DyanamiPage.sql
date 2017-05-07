4210466dbac9f7a5b1388cd0c0d9b944aa51e058
Tamang Binod <bakamaru_binod@hotmail.com>
2017-05-07 12:26:34 +0545

    html content script added

--- dbScript/DyanamiPage.sql
+++ dbScript/DyanamiPage.sql
@@ -9,9 +9,26 @@ CREATE TABLE dbo.Page
 	LastModified							datetime NOT NULL,
 	LastRequested							datetime NULL,
 	IsActive                                bit NOT NULL Default(1),
-    IsDeleted                               bit NOT NULL Default(0),
-    AddedOn                                 datetime NOT NULL Default(GETDATE()),
-    AddedBy                                 national character varying(256)
+    	IsDeleted                               bit NOT NULL Default(0),
+    	AddedOn                                 datetime NOT NULL Default(GETDATE()),
+    	AddedBy                                 national character varying(256)
 
 );
 CREATE INDEX idx_pge on dbo.Page(Location)
+
+CREATE TABLE dbo.HtmlContent
+(
+	HtmlContentId				bigint primary key identity(1,1),
+	KeyName					nvarchar(256) not null,
+	Content					nvarchar(MAX) ,
+	IsMarkDown				bit default(0) not null,
+	IsActive				bit default(1) not null,
+	IsDeleted				bit default(0) not null,
+	AddedOn					datetime default(getdate()),
+	AddedBy					nvarchar(256)
+);
+CREATE INDEX idx_htc on dbo.HtmlContent(KeyName)
+
+Insert into dbo.HtmlContent(KeyName,Content)values( 'test','<h1>hellow from test</h1>')
+
+
