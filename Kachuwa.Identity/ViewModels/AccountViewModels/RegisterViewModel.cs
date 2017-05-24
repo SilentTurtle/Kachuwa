656037a0687039d1ee5fb2fe3199a53f248e0a58
manoj joshi <contact.me.manoz@gmail.com>
2017-05-24 18:01:29 +0545

    RegisterViewModel changed

--- Kachuwa.Identity/ViewModels/AccountViewModels/RegisterViewModel.cs
+++ Kachuwa.Identity/ViewModels/AccountViewModels/RegisterViewModel.cs
@@ -19,5 +19,20 @@ namespace Kachuwa.Identity.ViewModels
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+        
+        [Required(ErrorMessage ="First name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "College is required.")]
+        public string College { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required.")]
+        public string PhoneNumber { get; set; }
     }
 }
