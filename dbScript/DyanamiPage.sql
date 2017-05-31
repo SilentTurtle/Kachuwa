502803607af69a905afd8582e3f2a1e5c1b601a7
Tamang Binod <bakamaru_binod@hotmail.com>
2017-05-31 12:35:09 +0545

    updated modeul

--- dbScript/DyanamiPage.sql
+++ dbScript/DyanamiPage.sql
@@ -2,33 +2,31 @@ CREATE TABLE dbo.Page
 (
 	PageId									bigint primary key identity(1,1) not null,
 	Name									nvarchar(256) NOT NULL,
-	URL										nvarchar(256),
-	Location								nvarchar(150) NOT NULL,
+	URL										nvarchar(256),	
 	Content									nvarchar(max) NOT NULL,
 	IsPublished								bit default(0) not null,
 	LastModified							datetime NOT NULL,
 	LastRequested							datetime NULL,
 	IsActive                                bit NOT NULL Default(1),
-    	IsDeleted                               bit NOT NULL Default(0),
-    	AddedOn                                 datetime NOT NULL Default(GETDATE()),
-    	AddedBy                                 national character varying(256)
+	IsDeleted                               bit NOT NULL Default(0),
+    AddedOn                                 datetime NOT NULL Default(GETDATE()),
+    AddedBy                                 national character varying(256)
 
 );
-CREATE INDEX idx_pge on dbo.Page(Location)
 
-CREATE TABLE dbo.HtmlContent
+CREATE TABLE dbo.Module
 (
-	HtmlContentId				bigint primary key identity(1,1),
-	KeyName					nvarchar(256) not null,
-	Content					nvarchar(MAX) ,
-	IsMarkDown				bit default(0) not null,
-	IsActive				bit default(1) not null,
-	IsDeleted				bit default(0) not null,
-	AddedOn					datetime default(getdate()),
-	AddedBy					nvarchar(256)
-);
-CREATE INDEX idx_htc on dbo.HtmlContent(KeyName)
+	ModuleId								int primary key identity(1,1) not null,
+	Name									nvarchar(256) not null,
+	Description								nvarchar(max) ,
+	Version									nvarchar(256) not null,
+	IsInstalled								bit default(0) not null,
+	Author									nvarchar(256),
+	IsActive                                bit NOT NULL Default(1),
+    IsDeleted								bit NOT NULL DEFAULT(0),
+    AddedOn									datetime NOT NULL DEFAULT(GETDATE()),
+    AddedBy									national character VARYING(256)
 
-Insert into dbo.HtmlContent(KeyName,Content)values( 'test','<h1>hellow from test</h1>')
+)
 
 
