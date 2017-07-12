fa7ac1192ad6949227ffbf4f0081cc3852c5b933
Tamang Binod <bakamaru_binod@hotmail.com>
2017-07-13 01:17:20 +0545

    updated image utility

--- Kachuwa.Web/Image/ImageUtility.cs
+++ Kachuwa.Web/Image/ImageUtility.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using System.IO;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Kachuwa.Web
