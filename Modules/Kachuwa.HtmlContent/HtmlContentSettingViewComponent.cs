d36daa6e267d4b3f4f89bfc72c06236a85ccdc98
Tamang Binod <bakamaru_binod@hotmail.com>
2017-08-22 14:55:11 +0545

    updated html module

--- Modules/Kachuwa.HtmlContent/HtmlContentSettingViewComponent.cs
+++ Modules/Kachuwa.HtmlContent/HtmlContentSettingViewComponent.cs
@@ -1,6 +1,9 @@
+using System.Threading.Tasks;
 using Kachuwa.HtmlContent.Service;
 using Kachuwa.Log;
+using Kachuwa.Web.Model;
 using Kachuwa.Web.Module;
+using Kachuwa.Web.Service;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kachuwa.HtmlContent
@@ -8,18 +11,29 @@ namespace Kachuwa.HtmlContent
     public class HtmlContentSettingViewComponent : KachuwaModuleViewComponent<HtmlModule>
     {
         private readonly IHtmlContentService _service;
+        private readonly ISettingService _settingService;
         private readonly ILogger _logger;
+        private readonly Setting _webSetting;
 
-        public HtmlContentSettingViewComponent(IHtmlContentService service, ILogger logger, IModuleManager moduleManager) : base(moduleManager)
+        public HtmlContentSettingViewComponent(IHtmlContentService service,ISettingService settingService, ILogger logger, IModuleManager moduleManager) : base(moduleManager)
         {
             _service = service;
+            _settingService = settingService;
+            _webSetting = _settingService.CrudService.Get(1);
             _logger = logger;
         }
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            _logger.Log(LogType.Info, () => "Invoking Html Setting Us View");
-            //var data = await _service.GetAll();
-            return View();
+           
+            var model =
+                await _service.HtmlService.GetListAsync("Where IsDeleted=0 and Culture=@Culture Order By Addedon desc",
+                    new
+                    {
+                        Culture = _webSetting.BaseCulture
+                    });
+
+
+            return View(model);
         }
 
         public override string DisplayName { get; } = "Html Content Setting";
