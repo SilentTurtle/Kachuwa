f6b03d3fdba79e831693a341269e6ede980afd1a
bakamaru <bakamaru_binod@hotmail.com>
2018-04-11 12:14:34 +0545

    updated gulp minifying theme

--- KachuwaApp/gulpfile.js
+++ KachuwaApp/gulpfile.js
@@ -2,12 +2,24 @@
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var bs = require('browser-sync').create();
+var rename = require('gulp-rename');
 
-gulp.task('sass', function () {
+gulp.task('compile_default', function () {
     return gulp.src('./themes/shared/default/sass/theme.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(sourcemaps.write('.'))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./themes/shared/default/css'))
+        .pipe(bs.reload({
+            stream: true
+        }));
+});
+gulp.task('compile_default_prod', function () {
+    return gulp.src('./themes/shared/default/sass/theme.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('./themes/shared/default/css'))
         .pipe(bs.reload({
             stream: true
@@ -15,28 +27,43 @@ gulp.task('sass', function () {
 });
  
 //Watch task
-gulp.task('default',['sass','frontendwtc']);
+gulp.task('default', ['compile_default', 'compile_default_prod', 'compile_admin', 'compile_admin_prod','watch']);
 
 
-gulp.task('compileadmin', function () {
+gulp.task('compile_admin', function () {
     return gulp.src('./themes/shared/admin/sass/theme.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./themes/shared/admin/css'))
         .pipe(bs.reload({
             stream: true
         }));
 });
+gulp.task('compile_admin_prod', function () {
+    return gulp.src('./themes/shared/admin/sass/theme.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./themes/shared/admin/css'))
+        .pipe(bs.reload({
+            stream: true
+        }));
+});
+
+
 //Watch task
-gulp.task('adminwtc',function() {
-    gulp.watch('./themes/shared/admin/**/*.scss',['compileadmin']);
+gulp.task('watch_admin_theme',function() {
+    gulp.watch('./themes/shared/admin/**/*.scss', ['compile_admin','compile_admin_prod']);
 });
  
 //Watch task
-gulp.task('frontendwtc',function() {
-    gulp.watch('./themes/shared/default/**/*.scss',['sass']);
+gulp.task('watch_default_theme',function() {
+    gulp.watch('./themes/shared/default/**/*.scss', ['compile_default','compile_default_prod']);
 });
+
+gulp.task('watch', ['watch_admin_theme', 'watch_default_theme']);
 
 
 
